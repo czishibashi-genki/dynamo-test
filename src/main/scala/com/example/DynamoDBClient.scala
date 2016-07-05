9779e8d279ecd68beb96de6a853feda33a2ab778
czishibashi-genki <ishibashi-genki@cyber-z.co.jp>
2016-07-05 19:49:08 +0900

    Update DynamoDBClient.scala

--- src/main/scala/com/example/DynamoDBClient.scala
+++ src/main/scala/com/example/DynamoDBClient.scala
@@ -7,7 +7,7 @@ import awscala.dynamodbv2._
 object DynamoDBClient {
   val TABLE_NAME = "fox2-dynamodb-test01"
   implicit val region = Region.AP_NORTHEAST_1
-  implicit val db = DynamoDB("AKIAJ33K5K6VNR7B56CA", "yBRFlrAD68Aw0Qy+5KxHAC4q9UFxFrtfKtVOLRF9")
+  implicit val db = DynamoDB("XXXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
   def put(hashPK: Any, attriubtes: (String, Any)*) =
     db.table(TABLE_NAME).getOrElse(throw new Exception)
